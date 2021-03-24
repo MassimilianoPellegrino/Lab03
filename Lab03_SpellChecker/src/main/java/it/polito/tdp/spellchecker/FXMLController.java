e3d08bb58fe55aaeb86d6e5ce987151d234d3de8
lenovo <lenovo@LAPTOP-2NKIQS9D.homenet.telecomitalia.it>
2021-03-24 09:18:18 +0100

    Base

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,23 +1,58 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private TextArea txtFrase;
+
+    @FXML
+    private TextArea txtParoleSbagliate;
+
+    @FXML
+    private Label lblErrori;
+
+    @FXML
+    private Label lblTempo;
+
+    @FXML
+    void doCheck(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doClear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doEnglish(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doItalian(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert txtFrase != null : "fx:id=\"txtFrase\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtParoleSbagliate != null : "fx:id=\"txtParoleSbagliate\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lblErrori != null : "fx:id=\"lblErrori\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lblTempo != null : "fx:id=\"lblTempo\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
